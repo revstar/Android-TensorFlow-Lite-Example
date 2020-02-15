ebb31fa1a8044f7115315d18f1ff80ba6561bde8
revstar <1967919189@qq.com>
2020-02-15 12:14:01 +0800

    模型导入成功

--- app/src/main/java/com/amitshekhar/tflite/Classifier.java
+++ app/src/main/java/com/amitshekhar/tflite/Classifier.java
@@ -1,78 +1,106 @@
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
 package com.amitshekhar.tflite;
 
 import android.graphics.Bitmap;
+import android.graphics.RectF;
 
 import java.util.List;
 
-/**
- * Created by amitshekhar on 17/03/18.
- */
-
+/** Generic interface for interacting with different recognition engines. */
 public interface Classifier {
+  List<Recognition> recognizeImage(Bitmap bitmap);
 
-    class Recognition {
-        /**
-         * A unique identifier for what has been recognized. Specific to the class, not the instance of
-         * the object.
-         */
-        private final String id;
+  void enableStatLogging(final boolean debug);
 
-        /**
-         * Display name for the recognition.
-         */
-        private final String title;
+  String getStatString();
 
-        /**
-         * Whether or not the model features quantized or float weights.
-         */
-        private final boolean quant;
+  void close();
 
-        /**
-         * A sortable score for how good the recognition is relative to others. Higher should be better.
-         */
-        private final Float confidence;
+  void setNumThreads(int num_threads);
 
-        public Recognition(
-                final String id, final String title, final Float confidence, final boolean quant) {
-            this.id = id;
-            this.title = title;
-            this.confidence = confidence;
-            this.quant = quant;
-        }
+  void setUseNNAPI(boolean isChecked);
 
-        public String getId() {
-            return id;
-        }
+  /** An immutable result returned by a Classifier describing what was recognized. */
+  public class Recognition {
+    /**
+     * A unique identifier for what has been recognized. Specific to the class, not the instance of
+     * the object.
+     */
+    private final String id;
 
-        public String getTitle() {
-            return title;
-        }
+    /** Display name for the recognition. */
+    private final String title;
 
-        public Float getConfidence() {
-            return confidence;
-        }
+    /**
+     * A sortable score for how good the recognition is relative to others. Higher should be better.
+     */
+    private final Float confidence;
 
-        @Override
-        public String toString() {
-            String resultString = "";
-            if (id != null) {
-                resultString += "[" + id + "] ";
-            }
+    /** Optional location within the source image for the location of the recognized object. */
+    private RectF location;
 
-            if (title != null) {
-                resultString += title + " ";
-            }
-
-            if (confidence != null) {
-                resultString += String.format("(%.1f%%) ", confidence * 100.0f);
-            }
-
-            return resultString.trim();
-        }
+    public Recognition(
+            final String id, final String title, final Float confidence, final RectF location) {
+      this.id = id;
+      this.title = title;
+      this.confidence = confidence;
+      this.location = location;
     }
 
+    public String getId() {
+      return id;
+    }
 
-    List<Recognition> recognizeImage(Bitmap bitmap);
+    public String getTitle() {
+      return title;
+    }
 
-    void close();
+    public Float getConfidence() {
+      return confidence;
+    }
+
+    public RectF getLocation() {
+      return new RectF(location);
+    }
+
+    public void setLocation(RectF location) {
+      this.location = location;
+    }
+
+    @Override
+    public String toString() {
+      String resultString = "";
+      if (id != null) {
+        resultString += "[" + id + "] ";
+      }
+
+      if (title != null) {
+        resultString += title + " ";
+      }
+
+      if (confidence != null) {
+        resultString += String.format("(%.1f%%) ", confidence * 100.0f);
+      }
+
+      if (location != null) {
+        resultString += location + " ";
+      }
+
+      return resultString.trim();
+    }
+  }
 }
--- app/src/main/java/com/amitshekhar/tflite/MainActivity.java
+++ app/src/main/java/com/amitshekhar/tflite/MainActivity.java
@@ -1,111 +1,148 @@
 package com.amitshekhar.tflite;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Parcelable;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.wonderkiln.camerakit.CameraKitError;
-import com.wonderkiln.camerakit.CameraKitEvent;
-import com.wonderkiln.camerakit.CameraKitEventListener;
-import com.wonderkiln.camerakit.CameraKitImage;
-import com.wonderkiln.camerakit.CameraKitVideo;
-import com.wonderkiln.camerakit.CameraView;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.view.CameraView;
 
+
+import com.luck.picture.lib.PictureSelector;
+import com.luck.picture.lib.config.PictureConfig;
+import com.luck.picture.lib.config.PictureMimeType;
+import com.luck.picture.lib.entity.LocalMedia;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String MODEL_PATH = "mobilenet_quant_v1_224.tflite";
-    private static final boolean QUANT = true;
-    private static final String LABEL_PATH = "labels.txt";
-    private static final int INPUT_SIZE = 224;
+    private static final String MODEL_PATH = "detect.tflite";
+    private static final boolean QUANT = false;
+    private static final String LABEL_PATH = "file:///android_asset/labels.txt";
+    private static final int INPUT_SIZE = 320;
 
     private Classifier classifier;
 
     private Executor executor = Executors.newSingleThreadExecutor();
     private TextView textViewResult;
-    private Button btnDetectObject, btnToggleCamera;
+    private Button btnToggleCamera;
     private ImageView imageViewResult;
-    private CameraView cameraView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        cameraView = findViewById(R.id.cameraView);
         imageViewResult = findViewById(R.id.imageViewResult);
         textViewResult = findViewById(R.id.textViewResult);
         textViewResult.setMovementMethod(new ScrollingMovementMethod());
 
         btnToggleCamera = findViewById(R.id.btnToggleCamera);
-        btnDetectObject = findViewById(R.id.btnDetectObject);
 
-        cameraView.addCameraKitListener(new CameraKitEventListener() {
+//        cameraView.addCameraKitListener(new CameraKitEventListener() {
+//            @Override
+//            public void onEvent(CameraKitEvent cameraKitEvent) {
+//
+//            }
+//
+//            @Override
+//            public void onError(CameraKitError cameraKitError) {
+//
+//            }
+//
+//            @Override
+//            public void onImage(CameraKitImage cameraKitImage) {
+//
+////                Bitmap bitmap = cameraKitImage.getBitmap();
+//
+//                Bitmap bitmap= BitmapFactory.decodeResource(getResources(),R.drawable.clothes);
+//                bitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false);
+//
+//                imageViewResult.setImageBitmap(bitmap);
+//
+//                final List<Classifier.Recognition> results = classifier.recognizeImage(bitmap);
+//
+//                textViewResult.setText(results.toString());
+//
+//            }
+//
+//            @Override
+//            public void onVideo(CameraKitVideo cameraKitVideo) {
+//
+//            }
+//        });
+//
+        btnToggleCamera.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onEvent(CameraKitEvent cameraKitEvent) {
-
+            public void onClick(View v) {
+                PictureSelector.create(MainActivity.this)
+                        .openGallery(PictureMimeType.ofImage())
+                        .loadImageEngine(GlideEngine.createGlideEngine()) // 请参考Demo GlideEngine.java
+                        .selectionMode(PictureConfig.SINGLE)
+                        .forResult(PictureConfig.CHOOSE_REQUEST);
             }
+        });
+//
+//        btnDetectObject.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                cameraView.captureImage();
+//            }
+//        });
 
-            @Override
-            public void onError(CameraKitError cameraKitError) {
-
+        initTensorFlowAndLoadModel();
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PictureConfig.CHOOSE_REQUEST) {
+            List<LocalMedia> localMediaList = PictureSelector.obtainMultipleResult(data);
+            if (localMediaList!=null&&localMediaList.get(0)!=null&&localMediaList.get(0).getPath()!=null){
+                progress(localMediaList.get(0).getPath());
             }
+        }
+    }
 
-            @Override
-            public void onImage(CameraKitImage cameraKitImage) {
-
-                Bitmap bitmap = cameraKitImage.getBitmap();
-
-                bitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false);
+    private void progress(String path){
+                Bitmap original= BitmapFactory.decodeFile(path);
+               Bitmap bitmap = Bitmap.createScaledBitmap(original, INPUT_SIZE, INPUT_SIZE, false);
 
                 imageViewResult.setImageBitmap(bitmap);
 
                 final List<Classifier.Recognition> results = classifier.recognizeImage(bitmap);
 
-                textViewResult.setText(results.toString());
-
-            }
-
-            @Override
-            public void onVideo(CameraKitVideo cameraKitVideo) {
-
-            }
-        });
-
-        btnToggleCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cameraView.toggleFacing();
-            }
-        });
-
-        btnDetectObject.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cameraView.captureImage();
-            }
-        });
-
-        initTensorFlowAndLoadModel();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        cameraView.start();
-    }
-
-    @Override
-    protected void onPause() {
-        cameraView.stop();
-        super.onPause();
+                if (results!=null&&results.get(0)!=null){
+                    textViewResult.setText(results.toString());
+                    RectF rectF=results.get(0).getLocation();
+                    if (rectF!=null){
+                        Bitmap drawRectBitmap=bitmap.copy(Bitmap.Config.ARGB_8888,true);
+                        Canvas canvas=new Canvas(drawRectBitmap);
+                        final Paint paint = new Paint();
+                        paint.setColor(Color.RED);
+                        paint.setStyle(Paint.Style.STROKE);
+                        paint.setStrokeWidth(2.0f);
+                        canvas.drawRect(rectF,paint);
+                        if (imageViewResult!=null){
+                            imageViewResult.setImageBitmap(drawRectBitmap);
+                        }
+                    }
+                }
     }
 
     @Override
@@ -124,25 +161,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    classifier = TensorFlowImageClassifier.create(
+                    classifier = TFLiteObjectDetectionAPIModel.create(
                             getAssets(),
                             MODEL_PATH,
                             LABEL_PATH,
                             INPUT_SIZE,
                             QUANT);
-                    makeButtonVisible();
                 } catch (final Exception e) {
                     throw new RuntimeException("Error initializing TensorFlow!", e);
                 }
-            }
-        });
-    }
-
-    private void makeButtonVisible() {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                btnDetectObject.setVisibility(View.VISIBLE);
             }
         });
     }
